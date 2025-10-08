734020b30aa83602c4e8ac98874b18360e05e716
gruizg <geshlee.ruiz@gmail.com>
2025-10-08 00:03:32 -0400

    Updated Navbar component: Current page now highlighted in Navbar.

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,21 +1,24 @@
+"use client"
 import styles from "@/app/components/navbar.module.css";
 import Link from "next/link";
-export default function navbar(){
+import {usePathname} from "next/navigation";
+export default function Navbar(){
+    const pathname = usePathname();
     return (
        <div className={styles.navbar}>
            <div className={styles.logo}>
-               <h1>RiverGuard</h1>
+               <Link href={"/"}><h1>RiverGuard</h1></Link>
            </div>
            <div className={styles.list}>
                <nav className={"navbar"}>
                    <ul>
-                       <li>
+                       <li className={pathname === "/about" ? styles.active : ""}>
                            <Link href={"/about"}>About Us</Link>
                        </li>
-                       <li>
+                       <li className={pathname === "/stats" ? styles.active : ""}>
                            <Link href={"/stats"}>Stats</Link>
                        </li>
-                       <li>
+                       <li className={pathname === "/donate" ? styles.active : ""}>
                            <Link href={"/donate"}>Donate</Link>
                        </li>
                    </ul>
